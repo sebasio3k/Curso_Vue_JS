4e2dbe4ba53418606f516fffdc627c7d89ec9d12
Sebastian Hernandez <bastianhdezo@gmail.com>
2020-09-10 20:34:44 -0500

    V33 Modulos import/export final"

---
+++ cursoJs/JS/constantes.js
@@ -0,0 +1,16 @@
+export const PI = Math.PI;
+
+export let usuario = "Seb";
+
+let password = "wesker";
+//export default password;
+
+export default function saludar() {
+  console.log("Hola modlos +ES6");
+}
+
+export class Saludar {
+  constructor() {
+    console.log("Hola clases +ES6");
+  }
+}
